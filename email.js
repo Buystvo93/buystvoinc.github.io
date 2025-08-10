ed740a6c3de2514070ea9518fceb36d705e90ec2
Buystvo93 <maxssho916@gmail.com>
2025-08-10 17:21:34 -0400

    yipee

--- email.js
+++
@@ -1,8 +0,0 @@
-function sendMail(){
-  let parms = {
-    name : document.getElementById("name").value,
-    message : document.getElementById("message").value
-  }
-
-  emailjs.send("service_h8n4lsw", "template_1pi2m8k", parms).then(alert("Email Sent!"))
-}
