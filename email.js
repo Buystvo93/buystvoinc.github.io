7bc2e5c5210068678582591fd9c8322b30db7d71
Buystvo93 <maxssho916@gmail.com>
2025-08-10 17:13:45 -0400

    Update email.js

--- email.js
+++ email.js
@@ -1,5 +1,8 @@
 function sendMail(){
   let parms = {
-    name : document.getElementById("username").value
+    name : document.getElementById("name").value,
+    message : document.getElementById("message").value
   }
+
+  emailjs.send("service_h8n4lsw", "template_1pi2m8k", parms).then(alert("Email Sent!"))
 }
